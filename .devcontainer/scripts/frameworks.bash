0063f4e86e54105fb436384c9c7826236dcf467e
Manoj M <manojmdev@proton.me>
2024-08-09 12:37:54 +0530

    added openrmf

--- .devcontainer/scripts/frameworks.bash
+++ .devcontainer/scripts/frameworks.bash
@@ -9,6 +9,8 @@ ros2_control=("sudo apt install ros-${ROS_DISTRO}-ros2-control ros-${ROS_DISTRO}
 # nav2
 nav2=("sudo apt install ros-${ROS_DISTRO}-navigation2 ros-${ROS_DISTRO}-nav2-bringup -y")
 
+# openrmf
+openrmf=("sudo apt install ros-${ROS_DISTOR}*")
 
 # plansys2
 plansys2=("sudo apt install ros-${ROS_DISTRO}-plansys2-* -y")
