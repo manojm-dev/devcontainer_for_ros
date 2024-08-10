c86152d2230e087ef238d195b9a535af5295b640
Manoj M <manojmdev@proton.me>
2024-08-10 12:17:20 +0530

    added some cli tools

--- .devcontainer/scripts/tools.bash
+++ .devcontainer/scripts/tools.bash
@@ -5,6 +5,8 @@
 # TERMINAL TOOLS
 bash_completion=('sudo apt install bash-completion -y')
 tmux=('sudo apt install tmux -y')
+tree=('sudo apt install tree -y')
+wormhole=('sudo apt install magic-wormhole -y')
 
 
 # C/C++ Dev tools
