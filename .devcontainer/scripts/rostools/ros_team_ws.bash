7e43c3b18c724fc5725af6d7701428f144bd50a8
Manoj M <manojmdev@proton.me>
2024-09-14 14:12:57 +0530

    added setup-auto-sourcing with color off in default setup

--- .devcontainer/scripts/rostools/ros_team_ws.bash
+++ .devcontainer/scripts/rostools/ros_team_ws.bash
@@ -6,8 +6,12 @@ location='/opt/ros_team_workspace'
 # Clone the repository
 git clone https://github.com/StoglRobotics/ros_team_workspace.git $location
 
+# Source ros_team_workspace
 echo '# sourcing ros_team_workspace' >> ~/.bashrc
 echo "source $location/setup.bash" >> ~/.bashrc
+
+# Setup auto sourcing without color
+echo -e "yes\nno" | setup-auto-sourcing
 
 # export rtw folder location
 echo "export RTW=\"$location\"" >> ~/.bashrc
